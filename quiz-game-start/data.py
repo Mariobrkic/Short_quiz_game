19c4a4a0ec362256a82b5c401dbb4f5119b953fa
Mario Brkić <51093050+Mariobrkic@users.noreply.github.com>
2023-08-05 02:33:17 +0200

    Add files via upload

---
+++ quiz-game-start/data.py
@@ -0,0 +1,50 @@
+question_data = [
+                {
+                    "category": "History",
+                    "type": "boolean",
+                    "difficulty": "medium",
+                    "question": "The two atomic bombs dropped on Japan by the United States in August 1945"
+                             " were named Little Man and Fat Boy.",
+                    "correct_answer": "False",
+                    "incorrect_answers": ["True"]},
+                {
+                    "category": "History", "type": "boolean", "difficulty": "medium",
+                    "question": "The first televised presidential debate was between Jimmy Carter and Gerald Ford.",
+                    "correct_answer": "False", "incorrect_answers": ["True"]},
+                {
+                    "category": "History", "type": "boolean", "difficulty": "medium",
+                    "question": "Sargon II, a king of the Neo-Assyrian Empire, was a direct descendant of "
+                              "Sargon of Akkad.",
+                   "correct_answer": "False", "incorrect_answers": ["True"]},
+                {
+                    "category": "History", "type": "boolean", "difficulty": "medium",
+                    "question": " 'I disapprove of what you say, but I will defend to the death your"
+                                " right to say it'; is a quote from French philosopher Voltaire.",
+                    "correct_answer": "False", "incorrect_answers": ["True"]},
+                {
+                    "category": "History", "type": "boolean", "difficulty": "medium",
+                    "question": "Theodore Roosevelt Jr. was the only General involved in the initial assault on D-Day.",
+                    "correct_answer": "True", "incorrect_answers": ["False"]},
+                {
+                    "category": "History", "type": "boolean", "difficulty": "medium",
+                    "question": "United States President Ronald Reagan was the first president to appoint"
+                                " a woman to the Supreme Court. ",
+                    "correct_answer": "True", "incorrect_answers": ["False"]},
+                {
+                    "category": "History", "type": "boolean", "difficulty": "medium",
+                    "question": "Oxford University is older than the Aztec Empire. ",
+                    "correct_answer": "True", "incorrect_answers": ["False"]},
+                {
+                    "category": "History", "type": "boolean", "difficulty": "medium",
+                    "question": "Adolf Hitler was accepted into the Vienna Academy of Fine Arts.",
+                    "correct_answer": "False", "incorrect_answers": ["True"]},
+                {
+                    "category": "History", "type": "boolean", "difficulty": "medium",
+                     "question": "Sir Issac Newton served as a Member of Parliament, but the only recorded time he spoke"
+                             " was to complain about a draft in the chambers.",
+                     "correct_answer": "True", "incorrect_answers": ["False"]},
+                {
+                    "category": "History", "type": "boolean", "difficulty": "medium",
+                    "question": "Franz Joseph I was the last emperor of Austria.",
+                    "correct_answer": "False", "incorrect_answers": ["True"]}
+                 ]
